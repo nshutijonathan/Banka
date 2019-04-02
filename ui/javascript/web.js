c10f83b5a6f7570603359fc0052edba57186d746
nshuti jonathan <nshutijonathan130@gmail.com>
2019-04-02 09:38:58 +0200

    Ft1 deactivate #165033118 (#55)
    * [ft1-log-in-#164921509]Added index page.
    
    * [ft1-log-in-#164921509]Designed index page.
    
    * [ft1-sign-up-#164931048]addes signup page.
    
    * [ft1-sign-up-#164931048]Added images folder.
    
    * [ft1-sign-up-#164931048]edited index.html
    
    * [ft1-sign-up-#164931048]added homepage.
    
    * [ft1-create-account-#164973107]Changed UI
    
    * Ft1 create account #164973107 (#8)
    
    * [ft1-log-in-#164921509]Added index page.
    
    * [ft1-log-in-#164921509]Designed index page.
    
    * [ft1-sign-up-#164931048]addes signup page.
    
    * [ft1-sign-up-#164931048]Added images folder.
    
    * [ft1-sign-up-#164931048]edited index.html
    
    * [ft1-sign-up-#164931048]added homepage.
    
    * [ft1-create-account-#164973107]Changed UI
    
    * [ft1-create-account-#164973107]Edited a photo.
    
    * [ft1-create-account-#164973107]added homepage.
    
    * Ft1 create account #164973107 (#9)
    
    * [ft1-log-in-#164921509]Added index page.
    
    * [ft1-log-in-#164921509]Designed index page.
    
    * [ft1-sign-up-#164931048]addes signup page.
    
    * [ft1-sign-up-#164931048]Added images folder.
    
    * [ft1-sign-up-#164931048]edited index.html
    
    * [ft1-sign-up-#164931048]added homepage.
    
    * [ft1-create-account-#164973107]Changed UI
    
    * [ft1-create-account-#164973107]Edited a photo.
    
    * [ft1-create-account-#164973107]added homepage.
    
    * [ft1-create-account-#164973107] added Drop down menu
    
    * Ft1 create account #164973107 (#10)
    
    * [ft1-log-in-#164921509]Added index page.
    
    * [ft1-log-in-#164921509]Designed index page.
    
    * [ft1-sign-up-#164931048]addes signup page.
    
    * [ft1-sign-up-#164931048]Added images folder.
    
    * [ft1-sign-up-#164931048]edited index.html
    
    * [ft1-sign-up-#164931048]added homepage.
    
    * [ft1-create-account-#164973107]Changed UI
    
    * [ft1-create-account-#164973107]Edited a photo.
    
    * [ft1-create-account-#164973107]added homepage.
    
    * [ft1-create-account-#164973107] added Drop down menu
    
    * [ft1-create-account-#164973107]Designed account creation form.
    
    * Ft1 create account #164973107 (#11)
    
    * [ft1-log-in-#164921509]Added index page.
    
    * [ft1-log-in-#164921509]Designed index page.
    
    * [ft1-sign-up-#164931048]addes signup page.
    
    * [ft1-sign-up-#164931048]Added images folder.
    
    * [ft1-sign-up-#164931048]edited index.html
    
    * [ft1-sign-up-#164931048]added homepage.
    
    * [ft1-create-account-#164973107]Changed UI
    
    * [ft1-create-account-#164973107]Edited a photo.
    
    * [ft1-create-account-#164973107]added homepage.
    
    * [ft1-create-account-#164973107] added Drop down menu
    
    * [ft1-create-account-#164973107]Designed account creation form.
    
    * [ft1-create-account-#164973107]Deleted unfunctional css.
    
    * [ft1-create-account-#164973107]added js validations.
    
    * [ft1-create-account-#164973107]Working folder restructured.
    
    * [ft1-create-account-#164973107]Working folder restructured.
    
    * [ft1-create-account-#164973107]Edited the link.
    
    * [ft1-view-transactions-#165021521]added transactions page.
    
    * [ft1-view-transactions-#165021521]added table of transactions.
    
    * [ft1-view-transactions-#165021521]added more transactions in the table.
    
    * [ft1-view-profile-#165030179]added user profile page.
    
    * [ft1-deactivate-#165033118]added admin and staff links
    
    * [ft1-deactivate-#165033118]linked profile page to all pages.
    
    * [ft1-deactivate-#165033118]added admin login link
    
    * [ft1-deactivate-#165033118]admin added deactivation button
    
    * [ft1-deactivate-#165033118]added js function to deactivate.

--- ui/javascript/web.js
+++ ui/javascript/web.js
@@ -1,3 +1,14 @@
 function create_account(){
     alert("check you email we sent you a link to proceed");
 }
+function deactivate(){
+	var retactivate=confirm("are you sure to deactivate this account?");
+	if(retactivate==true){
+		alert("User acount successfully deactivated!!");
+		return true;
+	}
+	else{
+		alert("User is still activated")
+		return false;
+	}
+}
