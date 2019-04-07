f753e857c48cad7fdd712149fb6bca3aac538514
jonathan nshuti <nshutijonathan130@gmail.com>
2019-04-07 19:46:21 +0200

    [ft2-sign-in-#165168227]added firstname and lastname validations.

--- server/helpers/validations.js
+++ server/helpers/validations.js
@@ -1,26 +1,40 @@
 import validator from 'validator';
 import user_db from '../models/users';
-
 class validateUser{
 	static validatesignup(req,res){
 		const allusers=user_db;
 		if (typeof req.body.email==='number') {
-			throw error("email must be in good format ");
+			throw Error("email must be in good format ");
 		}
 		if(validator.isEmpty(req.body.email)){
-			throw error("email is required")
+			throw Error("email is required")
+		}
+		if(!validator.isEmail(req.body.email)){
+			throw Error("your email must look like  this ex:andela@gmail.com");
 		}
 		if(validator.isEmpty(req.body.firstName)){
-			throw error("firstName is required");
+			throw Error("firstName is required");
+		}
+		if(!validator.isAlphanumeric(req.body.firstName)){
+			throw Error("firstName must not contain special characters!")
 		}
 		if(validator.isEmpty(req.body.lastName)){
-			throw error("lastName is required");
+			throw Error("lastName is required");
+		}
+		if(!validator.isAlphanumeric(req.body.lastName)){
+			throw Error("lastName must not contain special characters!")
 		}
 		if(!validator.isLength(req.body.password,{min:4,max:250})){
-			throw error("password must be at lease 4 characters")
+			throw Error("password must be at lease 4 characters")
 		}
 		if(validator.isEmpty(req.body.password)){
-			throw error("please confirm your password");
+			throw Error("please confirm your password");
+		}
+		if(validator.isEmpty(req.body.type)){
+			throw Error("type is required")
+		}
+		if (validator.isEmpty(req.body.isAdmin)) {
+			throw Error("the row isAdmin is required")
 		}
 		else{
 
@@ -30,3 +44,4 @@ class validateUser{
 	}
 
 }
+export default validateUser;
