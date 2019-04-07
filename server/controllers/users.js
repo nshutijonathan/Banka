2183bbe795399c72ba0b5ced38a64c3e259ec8d9
jonathan nshuti <nshutijonathan130@gmail.com>
2019-04-07 17:10:59 +0200

    [ft2-sign-up-#165141483]added routes.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,11 +1,71 @@
-import usersarray from '../models/users';
-import Users from '../models/users';
-class UsersController{
-	static getALLusers(req,res){
+import express from 'express';
+import users_db from '../models/users';
+
+class Userscontrollers{
+	static getAllusers(req,res){
 		return res.status(200).send({
-			message:"successfully",
-			usersarray
-		});
+		'status':200,
+		'message':"All users retrieved Successfully!",
+		users_db
+	});
 	}
+
+   static Usersignup(req,res){
+   	const data={
+		id:users_db.length + 1,
+		email:req.body.email,
+		firstName:req.body.firstName,
+		lastName:req.body.lastName,
+		password:req.body.password,
+        type:req.body.type,
+        isAdmin:req.body.isAdmin
+
+	}
+	users_db.push(data);
+	return res.status(201).send({
+		data
+
+	});
+
+   }
+   static getOneuser(req,res){
+   		const selectedUser = users_db.find(user => user.id === parseInt(req.params.id, 10));
+  if (selectedUser) {
+    return res.status(200).send({
+      status: 200,
+      data: [selectedUser]
+    });
+  }
+  return res.status(404).send({
+    status: 404,
+    error: `The user with the id ${req.params.id} was not found`
+  });
+   }
+   static deleteUser(req,res){
+   	const userId = req.params.id;
+  const userIndex = users_db.find(user => user.id === parseInt(userId, 10));
+  if(!userIndex){
+  	return res.status(404).send({error:`User with id ${userId} not found`});
+  }
+  const index=users_db.indexOf(userIndex);
+  users_db.splice(index,1);
+  return res.status(200).send({message:`User with id ${userId} Successfully deleted!`});
+   }
+   static updateUser(req,res){
+   	const userId = req.params.id;
+  const userIndex = users_db.find(user => user.id === parseInt(userId, 10));
+  if(!userIndex){
+  	return res.status(404).send({error:`User with id ${userId} not found`});
+  }
+  userIndex.email=req.body.email;
+  userIndex.firstName=req.body.firstName;
+  userIndex.lastName=req.body.lastName;
+  userIndex.password=req.body.password;
+  userIndex.type=req.body.type;
+  userIndex.isAdmin=req.body.isAdmin;
+  return res.status(200).send(userIndex);
+   };
 }
-export default UsersController;
+export default Userscontrollers;
+
+
--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import router from './routes/routes.js';
 import bodyParser from 'body-parser';
+import users_db from './models/users';
 const app=express();
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
+app.use(express.json());
 app.get('/',(req,res)=>{
 	return res.status(200).send({
 		'status':200,
-		'message':"Welcome to Banka!!"
+		'message':"Welcome to Banka!!",
 	});
 });
 const port=process.env.PORT ||3000;
