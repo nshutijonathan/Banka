5d0c8dcf383caad511711f4ea99570f64bfccbe0
jonathan nshuti <nshutijonathan130@gmail.com>
2019-04-12 12:21:54 +0200

    [ch-test-transactions-#165307943]added test [credit user endpoint].

--- server/tests/transactions.js
+++ server/tests/transactions.js
@@ -26,3 +26,94 @@ describe('get all transactions',()=>{
 
 	});
 });
+describe('debit a bank account',()=>{
+	it('Should be able to debit a bank account',(done)=>{
+		const transactions={
+			createdOn:date,
+			type:date,
+			accountNumber:"18299900",
+			amount:"2222",
+			oldBalance:"1222233",
+			newBalance:"3000000",
+
+		};
+		 chai.request(server)
+		.post('/api/v1/transactions/debit/:112345678')
+		.send(transactions)
+		.end((err,res)=>{
+			console.log(res.body);
+
+			res.body.should.be.an('object');
+			done();
+		});
+
+	});
+	it('Should not able to debit a bank account',(done)=>{
+		const transactions={
+			createdOn:date,
+			type:date,
+			accountNumber:"",
+			amount:"2222",
+			oldBalance:"1222233",
+			newBalance:"3000000",
+
+		};
+		 chai.request(server)
+		.post('/api/v1/transactions/debit/:112345678')
+		.send(transactions)
+		.end((err,res)=>{
+			console.log(res.body);
+
+			res.body.should.be.an('object');
+			done();
+		});
+
+	});
+});
+describe('Credit a bank account',()=>{
+	it('Should be able to credit a bank account',(done)=>{
+		const transactions={
+			createdOn:date,
+			type:date,
+			accountNumber:"18299900",
+			amount:"2222",
+			oldBalance:"1222233",
+			newBalance:"3000000",
+
+		};
+		 chai.request(server)
+		.post('//api/v1/transactions/credit/123456')
+		.send(transactions)
+		.end((err,res)=>{
+			console.log(res.body);
+
+			res.body.should.be.an('object');
+			done();
+		});
+
+	});
+	it('Should not able to credit a bank account',(done)=>{
+		const transactions={
+			createdOn:date,
+			type:date,
+			accountNumber:"56789",
+			amount:"2222",
+			oldBalance:"1222233",
+			newBalance:"3000000",
+
+		};
+		 chai.request(server)
+		.post('/api/v1/transactions/credit/1234')
+		.send(transactions)
+		.end((err,res)=>{
+			console.log(res.body);
+
+			res.body.should.be.an('object');
+			done();
+		});
+
+	});
+});
+	
+
+	
