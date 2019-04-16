f3df963368c794883f37505dd969b2cb56f0bc60
jonathan nshuti <nshutijonathan130@gmail.com>
2019-04-16 23:56:18 +0200

    [fb-ch1-signup-#165372370]validated debit user endpoint.

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -2,10 +2,14 @@ import express from 'express';
 import transactions from '../models/transactions';
 import users_db from '../models/users';
 import bank_accounts from '../models/create_bank_accounts';
+import validatetransactions from '../helpers/transactions';
 let date=new Date();
 //transactions class
 class Transactioncontrollers{
 	static createTransactions(req,res){
+		try{
+			if(validatetransactions.debit_credit(req,res)){
+
 		const accId = req.params.accountNumber;
         const check=bank_accounts.filter(account => account.accountNumber== accId);
         if(check==false){
@@ -23,7 +27,7 @@ class Transactioncontrollers{
 			id:transactions.length+1,
             createdOn:date,
             type:req.body.type,
-            accountNumber:req.body.accountNumber,
+            accountNumber:accId,
             cashier:req.body.cashier,
 			amount:req.body.amount,
 			oldBalance:req.body.oldBalance,
@@ -41,7 +45,17 @@ class Transactioncontrollers{
 			id,accountNumber,amount,cashier,type,newBalance
 		})
 	}
+}
+}
+catch(error){
+  console.log(error);
+    return res.status(400).send({
+      message:error.message
+    })
+   }
+
 	}
+
 	static getAlltransactions(req,res){
 		return res.status(200).send({
 			status:200,
@@ -68,7 +82,7 @@ class Transactioncontrollers{
 			id:transactions.length+1,
             createdOn:date,
             type:req.body.type,
-            accountNumber:req.body.accountNumber,
+            accountNumber:accId ,
             cashier:req.body.cashier,
 			amount:req.body.amount,
 			oldBalance:req.body.oldBalance,
@@ -76,7 +90,7 @@ class Transactioncontrollers{
 		}
 		transactions.push(data);
 		let id=data.id;
-		let accountNumber=data.accountNumber;
+		let accountNumber=req.params.accountNumber;
 		let amount=data.amount;
 		let cashier=data.cashier;
 		let type=data.type;
---
+++ server/helpers/transactions.js
@@ -0,0 +1,37 @@
+import  validator from 'validator';
+import transactions from '../models/transactions';
+
+class validatetransactions{
+	static debit_credit(req,res){
+		if(validator.isEmpty(req.body.type)){
+			throw Error("type is required")
+		}
+		if(validator.isEmpty(req.body.cashier)){
+			throw Error("cashier is required ");
+		}
+		if(validator.isEmpty(req.body.amount)){
+			throw Error("amount is required")
+		}
+		if(!validator.isNumeric(req.body.amount)){
+			throw Error("amount must be in numbers ");
+		}
+		if(validator.isEmpty(req.body.oldBalance)){
+			throw Error("oldBalance is required");
+		}
+		if(!validator.isNumeric(req.body.oldBalance)){
+			throw Error("oldBalance must be in numbers ");
+		}
+		if(validator.isEmpty(req.body.newBalance)){
+			throw Error("newBalance is required");
+		}
+		if(!validator.isNumeric(req.body.newBalance)){
+			throw Error("newBalance must be in numbers ");
+		}
+		else{
+
+		}
+		return true;
+
+	}
+}
+export default validatetransactions;
