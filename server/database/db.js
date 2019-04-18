f1745dc605210a22198c502f5af7f546b73413a3
jonathan nshuti <nshutijonathan130@gmail.com>
2019-04-18 09:54:32 +0200

    []Connected databse.

---
+++ server/database/db.js
@@ -0,0 +1,68 @@
+import pool from './connect';
+
+export const createTables=()=>{
+	const users=`CREATE TABLE IF NOT EXISTS
+	users(
+	id SERIAL PRIMARY KEY,
+	email VARCHAR(50) UNIQUE NOT NULL,
+	firstname VARCHAR(25) NOT NULL, 
+	lastname VARCHAR(25) NOT NULL,
+	password VARCHAR(25) NOT NULL,
+	type VARCHAR(10) NOT NULL,
+	isadmin VARCHAR(350) NOT NULL
+	)`;
+	const accounts=`CREATE TABLE IF NOT EXISTS
+	accounts(
+    id SERIAL PRIMARY KEY,
+    accountNumber INT NOT NULL,
+    createdOn VARCHAR(40) NOT NULL,
+    owner VARCHAR(20) NOT NULL,
+    type VARCHAR(10) NOT NULL,
+    status VARCHAR(10) NOT NULL,
+    balance INT  NOT NULL
+	)`;
+	const transactions=`CREATE TABLE IF NOT EXISTS
+	transactions(
+    id SERIAL PRIMARY KEY,
+    createdOn VARCHAR(20) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    accountNumber INT NOT NULL,
+    cashier VARCHAR(20) NOT NULL,
+    amount INT NOT NULL,
+    oldBalance INT NOT NULL,
+    newBalance INT NOT NULL
+	)`;
+	const queries=`${users}; ${accounts}; ${transactions}`;
+    pool.query(queries)
+    .then((res)=>{
+    	console.log(res);
+    	pool.end();
+    })
+    .catch((err)=>{
+    	console.log(err);
+    	pool.end();
+    });
+
+};
+export const dropTables=()=>{
+	const users='DROP TABLE IF EXISTS users';
+	const accounts='DROP TABLE IF EXISTS accounts';
+	const transactions='DROP TABLE IF EXISTS transactions'
+	pool.query(users);
+	pool.query(accounts);
+	pool.query(transactions)
+	.then((res)=>{
+		console.log(res);
+		pool.end();
+	})
+	.catch((err)=>{
+		console.log(err);
+		pool.end();
+	});
+	pool.on('remove',()=>{
+		console.log('client removed');
+		process.exit(0);
+
+	});
+};
+require('make-runnable');
