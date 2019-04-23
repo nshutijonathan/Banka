dfde839d74905009c71c6b5a3aa90f6a6b7eb185
jonathan nshuti <nshutijonathan130@gmail.com>
2019-04-23 23:45:00 +0200

    [ft3-create-user-as-staff-#165545610]auto generate account number.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,7 +9,7 @@ import accounts from '../controllers/create_bank_accountsv2';
 import auth from '../middlewares/authorizations';
 const {create,login,getall,getone,deleteuser}=User;
 const {createaccounts}=accounts;
-const {createstaff}=staff;
+const {createstaff,loginstaff}=staff;
 const usersv2Router=express.Router();
 const router=express.Router();
 //usersv2 endpoints
@@ -20,7 +20,7 @@ usersv2Router.get('/api/v2/clients/:id',auth.verifyToken,getone);
 usersv2Router.delete('/api/v2/clients/:id',auth.verifyToken,deleteuser);
 //Usersv2 create user as staff
 usersv2Router.post('/api/v2/auth/signup/staff',auth.verifyToken,createstaff);
-usersv2Router.post('/api/v2/auth/signin/staff',login);
+usersv2Router.post('/api/v2/auth/signin/staff',auth.verifyToken,loginstaff);
 //accountsv2 endpoints
 usersv2Router.post('/api/v2/accounts',auth.verifyToken,createaccounts);
 
